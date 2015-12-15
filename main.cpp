11af60cffc370ae773fa3cfb8f2c89c58085dc2c
nsifniotis <u5809912@anu.edu.au>
2015-12-15 14:34:35 +1100

    updating main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <boost/tokenizer.hpp>
+#include "Tokeniser/Tokeniser.h"
 
 using namespace std;
 
+
+string readFile(string);       // reads the contents of a textfile and returns them in the form of a vector
+                                            // whatever that is
+
+string readFile(string f)
+{
+    char *filename = &f[0];
+    string app;
+    string line;
+
+    ifstream inFile;
+    inFile.open(filename);//open the input file
+
+    stringstream strStream;
+    strStream << inFile.rdbuf();//read the file
+    app = strStream.str();//str holds the content of the file
+    inFile.close();
+
+    return app;
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    string file_contents = readFile("hello_world.N");
+
+    Tokeniser* tk = new Tokeniser(file_contents);
+    while (tk->more_data())
+    {
+        Token toke = tk->currentToken();
+        cout << toke.type << ": " << toke.data_payload << endl;
+    }
+
+    delete (tk);
 }
